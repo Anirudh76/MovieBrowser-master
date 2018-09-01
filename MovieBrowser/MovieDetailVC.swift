abd78a2b95364054f70919430d3b222b69da5c4d
brn.developers <brn.developers@achiever-3.local>
2018-09-01 15:23:09 +0530

    Initial Commit

---
+++ MovieBrowser/MovieDetailVC.swift
@@ -0,0 +1,41 @@
+//
+//  MovieDetailVC.swift
+//  MovieBrowser
+//
+//  Created by Azamuddin on 8/31/18.
+//  Copyright © 2018 Azamuddin. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailVC: UIViewController {
+    
+    
+    @IBOutlet weak var detailPoster: UIImageView!
+    @IBOutlet weak var detailOriginalTitle: UILabel!
+    @IBOutlet weak var detailRating: UILabel!
+    @IBOutlet weak var detailRelease: UILabel!
+    @IBOutlet weak var detailOverview: UITextView!
+    
+    var profile: UIImage?
+    var originalTitle: String?
+    var rating: String?
+    var release: String?
+    var overview: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        detailPoster.image = profile
+        detailOriginalTitle.text = originalTitle
+        detailRating.text = rating
+        detailRelease.text = release
+        detailOverview.text = overview
+        
+    }
+
+    
+
+  
+
+}
---
+++ MovieBrowser/MovieListCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListCell.swift
+//  MovieBrowser
+//
+//  Created by Azamuddin on 8/31/18.
+//  Copyright © 2018 Azamuddin. All rights reserved.
+//
+
+import UIKit
+
+class MovieListCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    @IBOutlet weak var titleLbl: UILabel!
+}
